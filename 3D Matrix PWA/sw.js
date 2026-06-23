8d311db50576bae8b14067a958b8bf253209910b
QuickByteMart <Quickbytemart@gmail.com>
2026-06-23 14:40:49 +0800

    Add files via upload

---
+++ 3D Matrix PWA/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'matrix-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js'
+];
+
+// Install: Cache the files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch: Serve from cache, fallback to network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
